5df973e3185d2b587fec58545fe459abad75d222
janeanekim <jmk90020@gmail.com>
2022-10-24 20:42:46 -0700

    added versioning and swagger api documentation

---
+++ src/main/java/com/tts/userapi/UserControllerV2.java
@@ -0,0 +1,121 @@
+package com.tts.userapi;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
+
+@Api(description="Users API Version 2.0")
+@RestController
+@RequestMapping("/v2")
+public class UserControllerV2 {
+	
+	@Autowired
+	UserRepository repository;
+	
+	//put in front of mapings to put them on the swagger ui
+	@ApiOperation(value = "Get user. State is required", response = User.class, 
+            responseContainer = "List")
+	@ApiResponses(value= {
+			@ApiResponse(code=200, message="Succefully accessed data."),
+			@ApiResponse(code=401, message="Unauthorized to view user data"),
+			@ApiResponse(code=403, message="Forbidden access to user data"),
+			@ApiResponse(code=404, message="Bad request, please try again with correct command"),
+	})
+	@GetMapping("/users")
+	public ResponseEntity<List<User>> getUsers(@RequestParam(value="state", required=true)String state){
+		if(state== null) {
+			List<User> users = repository.findAll();
+			return new ResponseEntity<>(users, HttpStatus.OK);
+		}
+		List<User> users=  repository.findByState(state);
+		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Get user by id number", response = User.class, 
+            responseContainer = "List")
+	@ApiResponses(value= {
+			@ApiResponse(code=200, message="Succefully accessed user data."),
+			@ApiResponse(code=401, message="Unauthorized to view user data"),
+			@ApiResponse(code=403, message="Forbidden access to user data"),
+			@ApiResponse(code=404, message="Bad request, please try again with correct command"),
+	})
+	@GetMapping("/users/{id}")
+	public ResponseEntity<Optional<User>> getUserById(@PathVariable(value="id") Long id){
+		Optional<User> users =repository.findById(id);
+		if(!users.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Add a user", response = User.class, 
+            responseContainer = "List")
+	@ApiResponses(value= {
+			@ApiResponse(code=200, message="Succefully added user data."),
+			@ApiResponse(code=401, message="Unauthorized to add user data"),
+			@ApiResponse(code=403, message="Forbidden mutation to user data"),
+			@ApiResponse(code=404, message="Bad request, please try again with correct command"),
+	})
+	@PostMapping("/users")
+	public ResponseEntity<Void> createUser(@Valid @RequestBody User user){
+		if(repository.findById(user.getId()).isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		repository.save(user);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@ApiOperation(value = "Update a user", response = User.class, 
+            responseContainer = "List")
+	@ApiResponses(value= {
+			@ApiResponse(code=200, message="Succefully updated user data."),
+			@ApiResponse(code=401, message="Unauthorized to update user data"),
+			@ApiResponse(code=403, message="Forbidden mutation to user data"),
+			@ApiResponse(code=404, message="Bad request, please try again with correct command"),
+	})
+	@PutMapping("/users/{id}")
+	public ResponseEntity<Void> updateUser(@Valid @PathVariable(value="id") Long id, @RequestBody User user){
+		if(repository.findById(user.getId()) == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		repository.save(user);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Delete a user", response = User.class, 
+            responseContainer = "List")
+	@ApiResponses(value= {
+			@ApiResponse(code=200, message="Succefully deleted user data."),
+			@ApiResponse(code=401, message="Unauthorized to delete user data"),
+			@ApiResponse(code=403, message="Forbidden mutation to user data"),
+			@ApiResponse(code=404, message="Bad request, please try again with correct command"),
+	})
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<Void> deleteUser(@Valid @PathVariable(value="id") Long id){
+		if(repository.findById(id).isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		repository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+}
